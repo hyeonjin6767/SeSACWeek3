f95c9c3dcf2bb45c4ee083939f3d35c74d8248ec
hyeonjin6767 <parkhj067670@gmail.com>
2025-07-14 10:04:26 +0900

    IQKeyboardManagerSwift 오픈소스 추가 및 UI Apperance

--- SeSACWeek3/AppDelegate.swift
+++ SeSACWeek3/AppDelegate.swift
@@ -7,13 +7,24 @@
 
 import UIKit
 
+import IQKeyboardManagerSwift //한줄 추가
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool { //앱에 빌드되는 최초시점에 모든 유아이에 적용되는
+        
+        
+        //(전체 ui에)앞으로 추가하는 모든 화면의 텍스트필드에 글자크기를 15, 배경 오렌지, 센터, 버튼 파란색을 변경 한다고 가정
+        UITextField.appearance().backgroundColor = .yellow
+        UITextField.appearance().font = .boldSystemFont(ofSize: 15)
+                
+        //1개의 뷰컨을 제외하고 모든 뷰컨에 키보드가 없다면? 이 위치는 적절하지 않다 //모든 뷰컨에 키보드가 있는 경우는 여기가 알맞
+        IQKeyboardManager.shared.isEnabled = true //키보드가 올라올 때 텍스트필드도 올라옴
+        
+        
         return true
     }
 
--- SeSACWeek3/ViewController.swift
+++ SeSACWeek3/ViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
